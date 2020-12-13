59a2926c4b45e646fea12ae5f9b40137d27b2635
WerWolv <werwolv98@gmail.com>
2020-12-13 23:58:49 +0100

    Implemented arrays and fixed memory issues

--- source/views/view_pattern.cpp
+++ source/views/view_pattern.cpp
@@ -290,6 +290,8 @@ namespace hex {
 
         hex::ScopeExit deleteAst([&ast]{ for(auto &node : ast.value()) delete node; });
 
+        return;
+
         hex::lang::Validator validator;
         auto validatorResult = validator.validate(ast.value());
         if (!validatorResult) {
@@ -305,7 +307,7 @@ namespace hex {
         }
         this->m_patternData = patternData.value();
 
-        this->postEvent(Events::PatternChanged);
+        //this->postEvent(Events::PatternChanged);
     }
 
 }
